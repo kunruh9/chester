d2810eebaf7f31fbaa755fa7d4dadaacfb991645
kunruh9 <kunruh9@gmail.com>
2020-12-05 10:58:07 -0700

    Add dotenv and fix exposed token

--- chester.py
+++ chester.py
@@ -1,4 +1,8 @@
 import discord
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class Chester(discord.Client):
@@ -14,11 +18,4 @@ class Chester(discord.Client):
 
 
 chester = Chester()
-# TODO: Update this token with the new one since I accidentally uploaded this one :facepalm:
-chester.run('Nzc4NDkxNTExMDcwMzkyMzIx.X7Swtw.DMZ97f6t_4eZBF7K5-5XMwHj56c')
-
-# notes on where I left off:
-# - Figuring out the best way/convention for interacting with environment variables in python
-# - Once that is figured out, I can set my token in the environment, and reference it above.
-# - Then I will be able to continue with the heroku "getting started" tutorial:
-#   - https://devcenter.heroku.com/articles/getting-started-with-python#deploy-the-app
+chester.run(os.environ.get('CHESTER_TOKEN'))
